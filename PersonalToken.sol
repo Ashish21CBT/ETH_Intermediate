22ea7338b7c5be2e8ca666e87e15cc641c0e6014
Ashish21CBT <136782875+Ashish21CBT@users.noreply.github.com>
2023-09-01 02:23:18 +0530

    Update PersonalToken.sol

--- PersonalToken.sol
+++ PersonalToken.sol
@@ -1,26 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-// Import SafeMath library
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+contract MyToken {
 
-contract PersonalToken {
-    using SafeMath for uint256;  // Use SafeMath for arithmetic operations
-
+    // public variables here
     string public tokenName = "Ashish";
     string public tokenAbbrv = "Ash";
-    uint256 public totalSupply = 0;
+    uint public totalSupply = 0;
 
-    mapping(address => uint256) public balances;
+    // mapping variable here
+    mapping(address => uint) public balances;
 
-    function mint(address account, uint256 value) public {
-        totalSupply = totalSupply.add(value); // Use SafeMath's add function
-        balances[account] = balances[account].add(value);
+    // mint function
+    function mint(address sender, uint value) public {
+        totalSupply += value;
+        balances[sender] += value;
     }
 
-    function burn(address account, uint256 value) public {
-        require(balances[account] >= value, "Insufficient balance");
-        totalSupply = totalSupply.sub(value); // Use SafeMath's sub function
-        balances[account] = balances[account].sub(value);
+     // burn function
+    function burn(address sender, uint value) public {
+        require(balances[sender] >= value, "Insufficient balance");
+        totalSupply -= value;
+        balances[sender] -= value;
     }
 }
